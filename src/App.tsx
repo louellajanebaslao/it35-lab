d177d3eeb97b20679d9fd12f897b5134dac5c8ce
louellajanebaslao <louellajanebaslao@gmail.com>
2025-02-13 06:17:37 +0800

    Reconfigure vite.config.ts and app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
@@ -39,11 +40,11 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route exact path="/home">
+        <Route exact path="/it35-lab/home">
           <Home />
         </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
+        <Route exact path="/it35-lab/">
+          <Redirect to="/it35-lab/home" />
         </Route>
       </IonRouterOutlet>
     </IonReactRouter>
