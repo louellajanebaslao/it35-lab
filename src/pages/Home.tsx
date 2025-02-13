d0b45631c65b61712cfa4ee97cd8bf50cd9fafbe
louellajanebaslao <louellajanebaslao@gmail.com>
2025-02-13 12:17:47 +0800

    added update to Menu/Login also Adding Sidemenu

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ import {
     IonToolbar 
 } from '@ionic/react';
 
-const Page2: React.FC = () => {
+const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
@@ -16,7 +16,7 @@ const Page2: React.FC = () => {
           <IonButtons slot='start'>
             <IonMenuButton></IonMenuButton>
           </IonButtons>
-          <IonTitle>Page2</IonTitle>
+          <IonTitle>Home</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -25,4 +25,4 @@ const Page2: React.FC = () => {
   );
 };
 
-export default Page2;
+export default Home;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,4 @@
+
 import { 
   IonButton,
   IonButtons,
