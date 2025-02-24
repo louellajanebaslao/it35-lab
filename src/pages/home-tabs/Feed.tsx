6c163fbac42ffba11bd09ba238c485f7095fa5e9
louellajanebaslao <louellajanebaslao@gmail.com>
2025-02-24 14:58:48 +0800

    Added ionic components

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,5 +1,10 @@
 import { 
   IonButtons,
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
     IonContent, 
     IonHeader, 
     IonMenuButton, 
@@ -7,7 +12,6 @@ import {
     IonTitle, 
     IonToolbar 
 } from '@ionic/react';
-
 const Feed: React.FC = () => {
   return (
     <IonPage>
@@ -20,19 +24,18 @@ const Feed: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-      <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100%',
-          }}
-        >
-          Feed
-        </div>
+      <IonCard>
+    <img alt="Silhouette of mountains" src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" />
+    <IonCardHeader>
+      <IonCardTitle>Card Title</IonCardTitle>
+      <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+    </IonCardHeader>
+
+    <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+  </IonCard>
+      
       </IonContent>
     </IonPage>
   );
 };
-
 export default Feed;
