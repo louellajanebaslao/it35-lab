35475c92b579d817cdaa123bacb430642a126ca0
louellajanebaslao <louellajanebaslao@gmail.com>
2025-04-16 13:11:39 +0800

    Updates

--- assets/swipe-back-legacy-BjNx1zEd.js
+++ assets/swipe-back-legacy-BjNx1zEd.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CMtoT22k.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-D_hSAe5D.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
