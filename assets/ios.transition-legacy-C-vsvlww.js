fb5ff1ab4f648a321dabf26657f805fe6344ab97
louellajanebaslao <louellajanebaslao@gmail.com>
2025-04-28 22:03:16 +0800

    Updates

--- assets/ios.transition-legacy-C-vsvlww.js
+++ assets/ios.transition-legacy-C-vsvlww.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D79Df3SF.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-Bssj4O1e.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/md.transition-legacy-pl3xklnY.js
+++ assets/md.transition-legacy-pl3xklnY.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D79Df3SF.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-Bssj4O1e.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
